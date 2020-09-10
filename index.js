200d7de4e4004649cfbc06348c899282afe1d454
danlei27 <danleisantos@hotmail.com>
2020-09-09 23:37:19 -0300

    rotas com parametros

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ app.get("/",function(req,res){
     res.send("Bem vindo a rota home!")
 })
 
-app.get("/blog",function(req,res){
-    res.send("Bem vindo ao meu blog!");
+app.get("/ola/:nome/:empresa",function(req, res){
+    //REQ => DADOS ENVIADOS PELO USUÁRIO
+    //RES => RESPOSTA QUE VAI SER ENVIADA PARA O USUÁRIO
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1>oi " + nome + " do " + empresa + "</h1>");
+});
+
+app.get("/blog/:artigo?",function(req,res){//parametro opcional(?)
+    var artigo = req.params.artigo;
+    if(artigo){
+        res.send("<h3>Artigo: " + artigo + "<h3/>");
+    }else{
+        res.send("Bem vindo ao meu blog!");
+    }
 })
 
 app.get("/canal/youtube",function(req,res){
