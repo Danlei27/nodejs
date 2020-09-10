8505d6534b0c673e6be5425d3f64c4f52489fe73
danlei27 <danleisantos@hotmail.com>
2020-09-09 21:21:40 -0300

    iniciando projeto npm

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ http.createServer(function(requisicao, resposta){
     resposta.end("<h1>Bem vindo ao meu site!<h1/>");
 }).listen(8181);
 
-console.log("Meu servidor está rodando!");
+console.log("Meu servidor está rodando!")
